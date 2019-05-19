7e46b4389558b0645a1057abf8a491386276b312
Paul Sinnett <paul.sinnett@gmail.com>
2019-05-19 22:12:42 +0100

    editor integration
    initialisation ensures that each editor camera is has the command buffer added

--- Assets/GrabGBuffer.cs
+++ Assets/GrabGBuffer.cs
@@ -3,16 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+[ExecuteInEditMode]
 public class GrabGBuffer : MonoBehaviour
 {
-	new Camera camera;
-	void Start()
+	static int instances = 0;
+	static Dictionary<Camera, CommandBuffer> cameras = new Dictionary<Camera, CommandBuffer>();
+
+	public void ConfigureCommandBuffers(Camera camera)
 	{
-		camera = GetComponent<Camera>();
-		CommandBuffer cb = new CommandBuffer();
-		int temp = Shader.PropertyToID("_MyGBufferCopy");
-		cb.GetTemporaryRT(temp, -1, -1);
-		cb.Blit(BuiltinRenderTextureType.GBuffer0, temp);
-		camera.AddCommandBuffer(CameraEvent.AfterGBuffer, cb); 
+		if (!cameras.ContainsKey(camera))
+		{
+			CommandBuffer commandBuffer = new CommandBuffer();
+			int copy = Shader.PropertyToID("_MyGBufferCopy");
+			commandBuffer.GetTemporaryRT(copy, -1, -1);
+			commandBuffer.Blit(BuiltinRenderTextureType.GBuffer0, copy);
+			camera.AddCommandBuffer(CameraEvent.AfterGBuffer, commandBuffer);
+			cameras.Add(camera, commandBuffer);
+		}
+	}
+
+	public void RemoveCommandBuffers()
+	{
+		foreach (Camera camera in cameras.Keys)
+		{
+			if (camera != null)
+			{
+				camera.RemoveCommandBuffer(CameraEvent.AfterGBuffer, cameras[camera]);
+			}
+		}
+		cameras.Clear();
+	}
+
+	public void OnEnable()
+	{
+		if (instances == 0)
+		{
+			Camera.onPreRender += ConfigureCommandBuffers;
+		}
+		instances++;
+	}
+
+	public void OnDisable()
+	{
+		instances--;
+		if (instances == 0)
+		{
+			Camera.onPreRender -= ConfigureCommandBuffers;
+			RemoveCommandBuffers();
+		}
 	}
 }
